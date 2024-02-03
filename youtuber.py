6b529a58f170b99fcbf413fa65d77886706a5153
CKBanka <chirag21142@iiitd.ac.in>
2024-02-03 07:48:38 +0530

    RabbitMQ initial testing phase

---
+++ youtuber.py
@@ -0,0 +1,18 @@
+import pika
+import sys
+import json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+youtuber = sys.argv[1]
+video_name = ' '.join(sys.argv[2:])
+
+def publish_video(youtuber, video_name):
+    channel.queue_declare(queue='youtuber_uploads')
+    body = json.dumps({'youtuber': youtuber, 'videoName': video_name})
+    channel.basic_publish(exchange='', routing_key='youtuber_uploads', body=body)
+    print("SUCCESS")
+
+publish_video(youtuber, video_name)
+connection.close()
